edafd9b96f4dbba4e607a5f8793c277f2f3ddee7
Vrajkumar Patel <31vrajpatel@gmail.com>
2025-06-25 13:31:56 +0530

    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import orderRouter from "./routes/orderRoute.js"
 
 // app config
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000;
 
 //middleware 
 app.use(express.json())
@@ -28,11 +28,11 @@ app.use("/api/user", userRouter)
 app.use("/api/cart", cartRouter)
 app.use("/api/order", orderRouter)
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
       res.send("API Working")
 })
 
-app.listen(port, ()=> {
+app.listen(port, () => {
       console.log(`Server started on http://localhost:${port}`)
 })
 
